8c0e7ad655fb35ca238b261923f5f5bfbe35a87d
allpino <alpinoacc2@hotmail.com>
2017-10-04 01:43:54 +0300

    menu
    menu

--- src/com/company/BulletDrop.java
+++ src/com/company/BulletDrop.java
@@ -1,26 +1,46 @@
 package com.company;
 
+import com.company.Screens.MenuScreen;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class BulletDrop extends JComponent {
 
+    static JFrame window;
+    static JPanel curScreen = null;
+
     public static void main(String[] args)
     {
-        JFrame window = new JFrame("Bullet Drop v0.1");
-        window.add(new BulletDrop());
+        window = new JFrame("Bullet Drop v0.1");
         window.pack();
+        BulletDrop bulletDrop = new BulletDrop();
+        window.add(bulletDrop);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
         window.setSize(new Dimension(Constants.GAME_WIDTH, Constants.GAME_HEIGHT));
         window.setResizable(false);
+
+        setCurScreen(new MenuScreen());
+
     }
+
+    public static void setCurScreen(JPanel screen)
+    {
+        curScreen = screen;
+        window.repaint();
+    }
+
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.drawString("Bullet Drop", 640,360);
+        if (curScreen != null)
+        {
+            curScreen.paintComponents(g);
+        }
+
     }
 }
---
+++ src/com/company/Screens/MenuScreen.java
@@ -0,0 +1,70 @@
+package com.company.Screens;
+
+
+import com.company.Constants;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class MenuScreen extends JPanel
+{
+    BufferedImage bq = null;
+    JPanel buttonPanel;
+
+    JButton newGameButton = new JButton("New Game");
+    JButton settings = new JButton("Setting");
+    JButton exit = new JButton("Exit");
+
+    public MenuScreen(){
+
+        try
+        {
+            bq = ImageIO.read(new File("C:\\Users\\AlpiN\\Desktop\\2F.Bullet-Drop\\src\\resources\\menu_bq.png"));
+        }
+        catch (IOException e)
+        {
+
+        }
+        if (bq == null)
+        {
+            throw new IllegalArgumentException("Could not load bq");
+        }
+
+        setVisible(true);
+
+        buttonPanel = new JPanel();
+
+        buttonPanel.setLayout(new FlowLayout());
+
+        newGameButton.setSize(600,400);
+        settings.setSize(600,400);
+        exit.setSize(600,400);
+
+        newGameButton.setVisible(true);
+        settings.setVisible(true);
+        exit.setVisible(true);
+
+    }
+
+
+
+    @Override
+    public void paintComponents(Graphics g)
+    {
+        super.paintComponents(g);
+
+        g.drawImage(bq,0,0,null);
+
+
+        buttonPanel.add(newGameButton);
+        buttonPanel.add(settings);
+        buttonPanel.add(exit);
+
+        add(buttonPanel);
+
+    }
+}
